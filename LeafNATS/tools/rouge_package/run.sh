40f5820c3908916b4fa114bb9b6c2a3ffb476b0f
Hao Phu Phan <haophan.cs.uit@gmail.com>
2021-05-10 11:16:00 +0300

    Update run.sh

--- LeafNATS/tools/rouge_package/run.sh
+++ LeafNATS/tools/rouge_package/run.sh
@@ -4,10 +4,10 @@ rm /mimicsql/ROUGE-1.5.5.tar.gz
 apt install -y libxml-parser-perl
 pip install pyrouge
 
-cd /mimicsql
+cd ~
 mkdir .pyrouge
 echo [pyrouge settings] >> .pyrouge/settings.ini
-echo home_dir = $PWD/ROUGE-1.5.5 >> .pyrouge/settings.ini
+echo home_dir = /mimicsql/ROUGE-1.5.5 >> .pyrouge/settings.ini
 
 cd /mimicsql/ROUGE-1.5.5/data/
 rm WordNet-2.0.exc.db
